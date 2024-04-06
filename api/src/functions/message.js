e6f6ff65571eaeff3ff99fed702b8d96f9fe22d0
MphaKheswa <siphamandlakheswa@outlook.com>
2024-04-06 16:57:08 +0200

    Update .gitignore to exclude .vscode/

---
+++ api/src/functions/message.js
@@ -0,0 +1,14 @@
+const { app } = require('@azure/functions');
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
