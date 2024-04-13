da34ab1b5511b0ac580dcd454bb0012c73bd9024
MphaKheswa <siphamandlakheswa@outlook.com>
2024-04-14 00:48:23 +0200

    Refactored Azure Static Web Apps CI/CD Prod workflow using Github Actions and removed unnecessary files

--- api/message/index.js
+++ api/message/index.js
@@ -1,23 +1,7 @@
-//module.exports = async function (context, req) {
-//    context.res.json({
-//        text: "Hello from the Azure Function API"
-//    });
-//};
+module.exports = async function (context, req) {
 
-const { app } = require('@azure/functions');
+    context.res.json({
+        text: "hello from the azure function api"
+    });
 
-app.http('httpTrigger1', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        var responseInJson = { body: `Hello, ${name}!` };
-
-        context.log(`Http function processed response object: "${responseInJson}"`);
-
-        return response;
-    }
-});
+};
