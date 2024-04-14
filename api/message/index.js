dcc154071466f20f4be7c2f5fed7597311909321
MphaKheswa <siphamandlakheswa@outlook.com>
2024-04-14 03:22:52 +0200

    Update model to v4

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,13 @@
-module.exports = async function (context, req) {
+const { app } = require('@azure/functions');
 
-    context.res.json({
-        text: "hello from the azure function api"
-    });
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
 
-};
+        const name = request.query.get('name') || (await request.text()) || 'world';
+
+        return { jsonBody : { body: `Hello, ${name}!` } };
+    },
+});
